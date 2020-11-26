b338be551f97f111a0eddcc6f1bf70144b381282
ray <zhulimin@baicgroup.com.cn>
2020-11-26 15:28:43 +0800

    merge and compelete one frame obs sel

---
+++ SRC/obstacle_selection/build/CMakeFiles/obstacle_selection.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "types.pb.cc"
+  "types.pb.h"
+  "obstacleSel.pb.cc"
+  "obstacleSel.pb.h"
+  "CMakeFiles/obstacle_selection.dir/test_selection.cc.o"
+  "CMakeFiles/obstacle_selection.dir/types.pb.cc.o"
+  "CMakeFiles/obstacle_selection.dir/obstacleSel.pb.cc.o"
+  "obstacle_selection.pdb"
+  "obstacle_selection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/obstacle_selection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
